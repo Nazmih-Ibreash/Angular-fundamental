963fee9f0b98c925c79ca66a0e98e12ab3098929
Nazmih-Ibreash <nazmihibreash@gmail.com>
2022-01-10 16:53:37 +0200

    ngTempSyntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+      <app-navbar></app-navbar>
+      <events-list></events-list>
+      `
 })
 export class AppComponent {
-  title = 'fundamental';
+  title = 'ng-funamental';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { EventsListComponent } from './events/events.component';
+import { EventThumbnailComponent } from './events/events-thumbnail.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    EventsListComponent,
+    EventThumbnailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
